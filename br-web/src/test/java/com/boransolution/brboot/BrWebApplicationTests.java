d2528ac061d856276f59944b026421294393a10b
xingyis <xingyis@yonyou.com>
2021-07-15 20:13:13 +0800

    second commit

---
+++ br-web/src/test/java/com/boransolution/brboot/BrWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boransolution.brboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
