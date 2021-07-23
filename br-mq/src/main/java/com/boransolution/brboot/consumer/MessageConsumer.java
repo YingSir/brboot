785bdfa6b5bb1efb37a836768a86a9c72af4a12b
xingyis <xingyis@yonyou.com>
2021-07-23 15:35:28 +0800

    add rabbitmq

--- br-mq/src/main/java/com/boransolution/brboot/consumer/MessageConsumer.java
+++ br-mq/src/main/java/com/boransolution/brboot/consumer/MessageConsumer.java
@@ -2,6 +2,7 @@ package com.boransolution.brboot.consumer;
 
 import com.boransolution.brboot.config.RabbitmqProducerConfig;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageConsumer {
     private RabbitTemplate rabbitTemplate;
-    //    @RabbitListener(queues = "com.boransolution.queue")
+
     @Autowired
     public MessageConsumer(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
@@ -18,5 +19,13 @@ public class MessageConsumer {
     public Message receiveMessage() {
         Message msg = rabbitTemplate.receive(RabbitmqProducerConfig.QUEUE_NAME);
         return msg;
+    }
+
+    /**
+     * 通过注解声明要监听订单消息，而不是使用RabbitTemplate进行轮询。
+     */
+    @RabbitListener(queues = "com.boransolution.queue")
+    public void listeningMessage(String msg) {
+        System.out.println(msg);
     }
 }
